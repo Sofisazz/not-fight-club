d11b538cd8d99f540f684077de516dcda86dfe77
Sofisazz <gmariya6666@gmail.com>
2025-08-19 21:59:55 +0300

    feat: update html, styles, script for character

--- script.js
+++ script.js
@@ -27,3 +27,34 @@ function closeEditCharacter(){
     edit.style.display='none';
     
 }
+
+function openEditModal(){
+    let popup = document.getElementById('popup');
+    popup.style.display='flex';
+}
+
+function chooseAvatar(item){   
+    let choose = item.querySelector('avatar-edit');
+    choose.style.display='flex';
+}
+
+function closeChooseAvatar(item){
+    let choose = item.querySelector('.avatar-edit');
+    choose.style.display='none';
+}
+
+
+function closeModal(){
+    let popup = document.getElementById('popup');
+    popup.style.display='none';
+}
+
+ function setAvatar(item) {
+        let avatar_item = item.closest('.avatar-item'); 
+        let avatar_image = avatar_item.querySelector('img'); 
+        let image_character = document.getElementById('image-character');
+        image_character.src = avatar_image.src; 
+
+        let popup = document.getElementById('popup');
+        popup.style.display = 'none'; 
+    }
