fcfee79608e036d22689932e8af3531d8d7791f5
Sofisazz <gmariya6666@gmail.com>
2025-08-20 21:39:31 +0300

    feat: update html, styles, script for battle

--- script.js
+++ script.js
@@ -2,17 +2,19 @@ let fight_section=document.getElementById('fight-section');
 let create_section=document.getElementById('create-section');
 let character_section=document.getElementById('character-section')
 let btn_create=document.getElementById('btn-create');
+let battle_section=document.getElementById('battle-section');
 
 function toFight(){
     fight_section.style.display='block';
     create_section.style.display='none';
     character_section.style.display='none';
+    battle_section.style.display='none'
     }
 
 function toCharacter(){
     fight_section.style.display='none';
     character_section.style.display='block';
-
+    battle_section.style.display='none'
     let name_character = document.getElementById('character-name');
     document.getElementById('avatar-name').innerText = name_character.value;
 }
@@ -57,4 +59,18 @@ function closeModal(){
 
         let popup = document.getElementById('popup');
         popup.style.display = 'none'; 
-    }
+}
+
+    function toBattle(){
+    let battle_my_name=document.getElementById('battle-my-name');
+    let name_character = document.getElementById('character-name');
+    let image_character = document.getElementById('image-character');
+    document.getElementById('image-avatar').src = image_character.src;
+
+    battle_my_name.innerText = name_character.value;
+
+    fight_section.style.display='none';
+    create_section.style.display='none';
+    character_section.style.display='none';
+    battle_section.style.display='block';
+}
